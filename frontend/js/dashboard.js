90fbd1364488c5cbe551ca9c5652b866fe5550f9
Hairii <dugarcein.leila16@gmail.com>
2026-03-04 11:31:11 +0100

    relier back front

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,6 +1,46 @@
 const token = sessionStorage.getItem("token");
-
-
 if (!token) {
-    window.location.href = "/authenticate";
+  window.location.href = "/authenticate";
 }
+
+const displayTask = (task) => {
+  const todo = document.getElementById("to-do");
+  const inprogress = document.getElementById("in-progress");
+  const finish = document.getElementById("finish");
+
+  [toDoEl, inProgressEl, finishEl].forEach(
+    (col) => (col.nextElementSibling.innerHTML = ""),
+  );
+
+  task.forEach((task) => {
+    const p = document.createElement("p");
+    p.className("task");
+    p.textContent = task.name;
+    switch (task.status) {
+      case 0:
+        todo.nextElementSibling.appendChild(p);
+        break;
+      case 1:
+        inprogress.nextElementSibling.appendChild(p);
+        break;
+      case 2:
+        finish.nextElementSibling.appendChild(p);
+        break;
+    }
+  });
+};
+
+const getTask = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/api/tasks/tasks", {
+      header: { Authorization: `Bearer ${token}` },
+    });
+
+    const task = await response.json();
+    displayTask(task);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getTask();
