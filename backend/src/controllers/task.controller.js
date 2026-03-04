5477a690de019c1c15f6a7a3045dbfb730b052e4
Hairii <dugarcein.leila16@gmail.com>
2026-03-04 10:03:52 +0100

    controller CRUD fini

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 import { createTask, getAllTasks } from "../../../frontend/Js/From";
+import { update } from "./auth.controller";
 
 // recuperer les taches
 export const fetchTasks = async (req, res)=>{
     try{
-        const userID = getUserID(req);
+       
         const task =  await getAllTasks(userID);
         res.json(task);
     } catch(error) {
@@ -17,7 +18,7 @@ export const fetchTasks = async (req, res)=>{
 //creer une tache
 export const addTask = async (req, res)=> {
     try{
-        const userID = getUserID(req);
+
         const  {name, status} = req.body;
         const id = await createTask({name, userID,status: status ?? 0});
         res.status(201).json ({message:"Tache créée", id})
@@ -25,4 +26,30 @@ export const addTask = async (req, res)=> {
         console.error(error);
         res.status(500).json({message: "Erreur serveur (addTask)"})
     }
-}
+};
+
+// changer un tache
+export const editTask = async (req, res)=>{
+    try{
+        const {id}= req.params;
+        const {name, status} = req.body;
+        await updateTask (id,{name, status});
+        res.json ({message: "tache modifier"});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: "Erreur serveur (editTask)"})
+    }
+};
+
+//supprimer tache
+export const removeTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await deleteTask(id);
+        if (!deleted) return res.status(404).json({ message: "Tâche non trouvée" });
+        res.json({ message: "Tâche supprimée" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur removeTask" });
+    }
+};
