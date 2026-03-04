29b233f388b79090568fb3ec46ce79995d6e65d6
Hairii <dugarcein.leila16@gmail.com>
2026-03-04 15:22:47 +0100

    btn delete fonctionne

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -7,16 +7,17 @@ const displayTask = (tasks) => {
     (col) => (col.nextElementSibling.innerHTML = "")
   );
 
-  tasks.forEach((task, index) => {
+  tasks.forEach((task) => {
     const div = document.createElement("div");
     div.className = "task";
 
     // texte + boutons
     div.innerHTML = `
-      <span>${task.name}</span>
+      <p>${task.name}</p>
       <div class="btn_action">
-        <button onclick="editTask(${index})">Modifier</button>
-        <button onclick="deleteTask(${index})">Supprimer</button>
+      <button onclick="deleteTask(${task.id})">Supprimer</button>
+      <button onclick="editTask">--></button>
+        
       </div>
     `;
 
@@ -39,8 +40,8 @@ const getTask = async () => {
   try {
    
     const test=localStorage.getItem("userID")
-    console.log(test);
-    const response = await fetch(`http://localhost:5000/api/tasks/tasks/${test}`, {
+    console.log(userID);
+    const response = await fetch(`http://localhost:5000/api/tasks/tasks/${userID}`, {
       
     });
 
@@ -50,5 +51,24 @@ const getTask = async () => {
     console.error(error);
   }
 };
+
+
+const deleteTask = async(id)=> {
+
+   try {
+    console.log(id);
+    const response = await fetch(`http://localhost:5000/api/tasks/deletetasks/${id}`, {
+      method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+
+    });
+    const task = await response.json();
+    
+     } catch (error) {
+    console.error(error);
+  }
+};
+
 
 getTask();
