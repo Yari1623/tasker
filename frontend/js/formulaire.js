b467745566ba46c702180ba082e73e09baf10a84
Yari1623 <yannick.richard.afec.formation@outlook.fr>
2026-03-04 16:36:56 +0100

    formulaire

--- frontend/js/formulaire.js
+++ frontend/js/formulaire.js
@@ -1,16 +1,27 @@
-const getTask = async () => {
+const backdash = document.getElementById("back-btn");
+
+backdash.addEventListener('click', () => {
+    window.location.href = "/dashboard";
+}
+)
+
+const nom = document.getElementById("title").value;
+
+const createTask
+ = async () => {
   try {
    
-    const test=localStorage.getItem("userID")
+    const userID=localStorage.getItem("userID")
     console.log(userID);
     const response = await fetch(`http://localhost:5000/api/tasks/addtasks/${userID}`, {
       method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({nom}),
             credentials: 'include',
     });
 
     const task = await response.json();
-    displayTask(task);
+  
   } catch (error) {
     console.error(error);
   }
